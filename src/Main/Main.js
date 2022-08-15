74feb48885990d4c0ff1b7fc9818cfd768abd652
Mariana Machohan <debrisofme9@gmail.com>
2022-08-15 15:04:21 +0200

    +p

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,6 +1,7 @@
 function Main() {
   return (
     <>
+			<p>Text example</p>
       {/* <Header data={headerData} />
       <Test />
       {goods.map(item => <Goods title={item.title} cost={item.cost} image={item.image} />)} */}
