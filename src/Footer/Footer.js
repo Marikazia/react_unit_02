7b320c74dad7fb896821fe93c5fb16d2d073d54f
Mariana Machohan <debrisofme9@gmail.com>
2022-08-16 13:59:19 +0200

    +

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,6 +1,8 @@
 function Footer() {
   return (
     <>
+			<h3>site_name</h3>
+			<Nav nav = {props.data.nav} />
       {/* <Header data={headerData} />
       <Test />
       {goods.map(item => <Goods title={item.title} cost={item.cost} image={item.image} />)} */}
