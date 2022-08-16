c28d7de4ef16688eaae3a79f136a44e6dc97b581
Mariana Machohan <debrisofme9@gmail.com>
2022-08-16 14:56:58 +0200

    +

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,9 +1,12 @@
+import Nav from '../Header/Nav';
+
 function Footer(props) {
+	// let data = props.footer;
   return (
-    <>
+    <footer>
 			<h3>site_name</h3>
 			<Nav nav = {props.data.nav} />
-    </>
+    </footer>
   );
 }
 
