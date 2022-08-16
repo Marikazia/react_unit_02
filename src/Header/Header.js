38a6e656c0b57a6fa97396e7336b4ca90a166795
Mariana Machohan <debrisofme9@gmail.com>
2022-08-16 13:58:49 +0200

    +

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,23 +1,24 @@
 function Header(props) {
-    return (
-        <header>
-            <h1>{props.data.site_name}</h1>
-            <h2>{props.second_header}</h2>
-            <Nav nav={props.data.nav} />
-        </header>
-    );
+	return (
+		<header>
+			111
+			<h1>{props.data.site_name}</h1>
+			<h2>{props.second_header}</h2>
+			<Nav nav = {props.data.nav} />
+		</header>
+	);
 }
 
 function Nav(props) {
-    let data = props.nav;
-    const listItem = data.map(item => <li key={item.link}><a href={item.link}>{item.text}</a></li>);
-    return (
-        <nav>
-            <ul>
-                {listItem}
-            </ul>
-        </nav>
-    );
+	let data = props.nav;
+	const listItem = data.map(item => <li key={item.link}><a href={item.link}>{item.text}</a></li>);
+	return (
+		<nav>
+			<ul >
+				{listItem}
+			</ul>
+		</nav>
+	);
 }
 
 export default Header;
