06284c00e604746a3e447db37914ac77cc1e2958
Mariana Machohan <debrisofme9@gmail.com>
2022-08-16 13:58:58 +0200

    +

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,12 +1,20 @@
-function Main() {
+function Main(props) {
   return (
     <>
 			<p>Text example</p>
-      {/* <Header data={headerData} />
-      <Test />
-      {goods.map(item => <Goods title={item.title} cost={item.cost} image={item.image} />)} */}
+			<Sidebar sidebar_ul = {props.data.sidebar_ul} />
     </>
   );
+}
+
+function Sidebar(props) {
+	let data = props.sidebar_ul;
+	const listItem = data.map(item => <li key={item.link}><a href={item.link}>{item.text}</a></li>);
+	return (
+		<ul>
+			{listItem}
+		</ul>
+	);
 }
 
 export default Main;
