62d80930bc8c9756d11207e1dc688a966b8b3302
Mariana Machohan <debrisofme9@gmail.com>
2022-08-16 14:57:12 +0200

    +

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,3 +1,5 @@
+import Sidebar from './Sidebar';
+
 function Main(props) {
   return (
     <>
@@ -5,16 +7,6 @@ function Main(props) {
 			<Sidebar sidebar_ul = {props.data.sidebar_ul} />
     </>
   );
-}
-
-function Sidebar(props) {
-	let data = props.sidebar_ul;
-	const listItem = data.map(item => <li key={item.link}><a href={item.link}>{item.text}</a></li>);
-	return (
-		<ul>
-			{listItem}
-		</ul>
-	);
 }
 
 export default Main;
